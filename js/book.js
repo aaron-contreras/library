07f390f05e5e4f8e95d089134c382cf12f901c79
Aaron Contreras <61627014+aaron-contreras@users.noreply.github.com>
2022-05-12 08:21:41 -0500

    refactor(book): switch constructor syntax for class syntax (#15)
    As a practice in using ES6 classes, refactored the Book constructor into a class based object.
    Exposing fields only via getters and adding privacy to properties. Also, the only setter needed is
    for the `read` status, so exposed `toggleRead` as the setter for it.

--- js/book.js
+++ js/book.js
@@ -1,17 +1,50 @@
-function Book(attrs) {
-  this.title = attrs.title;
-  this.author = attrs.author;
-  this.pages = attrs.pages;
-  this.read = attrs.read;
-  this.coverColor = bookCoverColors[Math.floor(Math.random() * bookCoverColors.length)];
+class Book {
+  // Properties
+  #title;
+  #author;
+  #pages;
+  #read;
+  #coverColor;
 
-  this.info = function() {
+  constructor(attrs) {
+    this.#title = attrs.title;
+    this.#author = attrs.author;
+    this.#pages = attrs.pages;
+    this.#read = attrs.read;
+    this.#coverColor = bookCoverColors[Math.floor(Math.random() * bookCoverColors.length)];
+  }
+
+  // Getters
+  get title() {
+    return this.#title;
+  }
+
+  get author() {
+    return this.#author;
+  }
+
+  get pages() {
+    return this.#pages;
+  }
+
+  get read() {
+    return this.#read;
+  }
+  
+  get coverColor() {
+    return this.#coverColor;
+  }
+
+  // Methods
+  toggleRead() {
+    this.#read = !this.#read;
+  }
+
+  // Not used in UI
+  info() {
     readStatus = this.read ? 'read' : 'not read yet';
     
     return `${this.title} by ${this.author}, ${this.pages} pages, ${readStatus}`;
   }
-
-  this.toggleRead = () => {
-    this.read = !this.read;
-  }
+  // Not used in UI
 }
